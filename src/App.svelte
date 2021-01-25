45ae14010524d46e48e4fcc556ce2f929f95df59
Danko Lučić <ld.is.a.git@gmail.com>
2021-01-25 23:10:48 +0100

    - added some speed controls - removed unnecessary css

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,21 @@ import gameStateStore from './gameStateStore';
 import Field from './Field.svelte';
 
 let gameState;
+let speed = 7;
+const speedMap = [
+  { delay: 500, description: 'Dead snail on a slope' },
+  { delay: 400, description: 'Snail' },
+  { delay: 350, description: 'Snail running from a bird' },
+  { delay: 300, description: 'Lethargic turtle' },
+  { delay: 250, description: 'Turtle' },
+  { delay: 200, description: 'Turtle on a skateboard' },
+  { delay: 130, description: 'Regular' },
+  { delay: 100, description: 'Fast' },
+  { delay: 70, description: 'Cheetah' },
+  { delay: 50, description: 'Cheetah driving a Porsche' },
+  { delay: 10, description: 'Bolt of Lighting' },
+];
+
 gameStateStore.subscribe(newGameState => (gameState = newGameState));
 function handleKeypress(event) {
   const key = event.key.toLowerCase();
@@ -19,7 +34,12 @@ function handleKeypress(event) {
 }
 
 document.addEventListener('keypress', handleKeypress);
-const intervalId = setInterval(gameStateStore.moveSnake, 50);
+let intervalId = setInterval(gameStateStore.moveSnake, speedMap[speed].delay);
+
+$: {
+  clearInterval(intervalId);
+  intervalId = setInterval(gameStateStore.moveSnake, speedMap[speed].delay);
+}
 
 onDestroy(() => {
   document.removeEventListener('keypress', handleKeypress);
@@ -30,6 +50,10 @@ onDestroy(() => {
 <main>
   <h1>Snake</h1>
   <Field {gameState} />
+  <div class="controls">
+    <span>Speed: {speedMap[speed].description}</span>
+    <input type="range" bind:value={speed} min="0" max={speedMap.length - 1} step="1" />
+  </div>
 </main>
 
 <style>
@@ -54,5 +78,15 @@ h1 {
   main {
     max-width: none;
   }
+}
+
+.controls {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.controls span {
+  width: 300px;
 }
 </style>
