1f11a64ddb9fda25c612d0129f7158103e6d7aba
Danko Lučić <ld.is.a.git@gmail.com>
2021-01-30 15:28:25 +0100

    more speed levels and speed tweaks

--- src/core/speedLevels.js
+++ src/core/speedLevels.js
@@ -1,16 +1,18 @@
 // Order is important
 export const speedMap = [
-  { delay: 680, description: 'Dead snail on a slope' },
-  { delay: 510, description: 'Snail' },
-  { delay: 380, description: 'Snail running from a bird' },
-  { delay: 300, description: 'Turtle depressed by the state of the world' },
+  { delay: 300000, description: '"Need to make some coffee" speed' }, // cca. 5 minutes between moves
+  { delay: 700, description: 'Dead snail on a slope' },
+  { delay: 540, description: 'Snail' },
+  { delay: 400, description: 'Sloth' },
+  { delay: 320, description: 'Snail running from a bird' },
   { delay: 230, description: 'Turtle' },
   { delay: 160, description: 'Turtle on a skateboard' },
   { delay: 80, description: 'Regular speed' },
   { delay: 50, description: 'Fast' },
   { delay: 30, description: 'Cheetah' },
   { delay: 20, description: 'Cheetah driving a Porsche' },
-  { delay: 10, description: 'Bolt of Lighting' },
+  { delay: 10, description: 'M-497 Black Beetle' },
+  { delay: 4, description: 'Warp 11' }, // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval#delay_restrictions
 ];
 
 export function getDefaultSpeed() {
