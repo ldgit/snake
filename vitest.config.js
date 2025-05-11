8eb1ec9dbf2850540321bf211718e9c43974963e
Danko Lučić <ld.is.a.git@gmail.com>
2025-05-11 20:51:22 +0200

    fix vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelteTesting } from '@testing-library/svelte/vite';
 
 export default defineConfig({
   // eslint-disable-next-line no-undef
-  plugins: [svelte({ hot: !process.env.VITEST })],
+  plugins: [svelte({ hot: !process.env.VITEST }), svelteTesting()],
   test: {
+    environment: 'jsdom',
     globals: false,
   },
 });
